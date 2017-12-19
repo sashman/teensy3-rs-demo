de751f240c0037b116c05ca4f32714bb59b1a09c
sashman <sashman90@gmail.com>
2017-12-19 09:47:52 +0000

    Updating to compile

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 build:
-	@xargo build --release
+	xargo build --release
 
 flash: build
 	arm-none-eabi-objcopy -O ihex -R .eeprom target/thumbv7em-none-eabi/release/teensy3-rs-demo target/hex
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 #![no_std]
 #![no_main]
+#![feature(use_extern_macros)]
 
-#[macro_use]
+#[macro_use(print)]
 extern crate teensy3;
 
 use teensy3::bindings;
@@ -10,18 +11,19 @@ use teensy3::serial::Serial;
 #[no_mangle]
 pub unsafe extern fn main() {
     // Blink Loop
-
     bindings::pinMode(13, bindings::OUTPUT as u8);
     bindings::digitalWrite(13, bindings::LOW as u8);
-    let mut ser = Serial{};
+    let _ser = Serial{};
+    let mut count: i8 = 0;
 
     loop {
         // Show we are alive
         alive();
 
         // If the serial write fails, we will halt (no more alive blinks)
-        hello(&ser).unwrap();
-
+        send_count(count).unwrap();
+        // send_count(&ser, count).unwrap();
+        count += 1;
         // Don't spam the console
         bindings::delay(1000);
     }
@@ -31,16 +33,26 @@ pub unsafe extern fn main() {
 pub unsafe fn alive() {
     for _ in 0..2 {
         bindings::digitalWrite(13, bindings::LOW as u8);
-        bindings::delay(200);
+        bindings::delay(100);
         bindings::digitalWrite(13, bindings::HIGH as u8);
-        bindings::delay(200);
+        bindings::delay(100);
         bindings::digitalWrite(13, bindings::LOW as u8);
-        bindings::delay(200);
+        bindings::delay(100);
     }
 }
 
 /// Send a message over the USB Serial port
-pub fn hello(ser: &Serial) -> Result<(),()> {
-    let msg = "Hello Teensy Rusty World!\n\r";
-    ser.write_bytes(msg.as_bytes())
+pub fn hello() -> Result<(),()> {
+    print!("H\n");
+    Ok(())
+}
+
+pub fn send_count(count: i8) -> Result<(),()> {
+    print!("{}\n", count);
+    Ok(())
+}
+
+pub fn send_echo(msg : u8) -> Result<(),()> {
+    print!("Received: {}\n", msg);
+    Ok(())
 }
