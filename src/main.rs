1641a711c3e36e0d028981b5fd88ca186d638d78
sashman <sashman90@gmail.com>
2017-12-19 14:02:16 +0000

    Adding a listen for byte in main loop

--- src/main.rs
+++ src/main.rs
@@ -13,19 +13,24 @@ pub unsafe extern fn main() {
     // Blink Loop
     bindings::pinMode(13, bindings::OUTPUT as u8);
     bindings::digitalWrite(13, bindings::LOW as u8);
-    let _ser = Serial{};
-    let mut count: i8 = 0;
+    let ser = Serial{};
+    let mut count: i32 = 0;
 
     loop {
         // Show we are alive
         alive();
 
         // If the serial write fails, we will halt (no more alive blinks)
-        send_count(count).unwrap();
-        // send_count(&ser, count).unwrap();
+        send_waiting(count).unwrap();
+
+        match ser.try_read_byte() {
+            Some(in_byte) => send_echo(in_byte),
+            None => Ok(())
+        }.unwrap();
+
         count += 1;
         // Don't spam the console
-        bindings::delay(1000);
+        bindings::delay(100);
     }
 }
 
@@ -42,17 +47,12 @@ pub unsafe fn alive() {
 }
 
 /// Send a message over the USB Serial port
-pub fn hello() -> Result<(),()> {
-    print!("H\n");
-    Ok(())
-}
-
-pub fn send_count(count: i8) -> Result<(),()> {
-    print!("{}\n", count);
+pub fn send_waiting(count: i32) -> Result<(),()> {
+    print!("Waiting {}\n\r", count);
     Ok(())
 }
 
 pub fn send_echo(msg : u8) -> Result<(),()> {
-    print!("Received: {}\n", msg);
+    print!("Received: \"{}\"\n\r", msg as char);
     Ok(())
 }
